e8727bea0c8331d059200fd68d2ca85ba4a17677
Bjørn Karmann <bjoern.t.karmann@gmail.com>
2017-06-09 19:26:43 +0200

    updated code sender

--- SW/client/scripts/GcodeSender.js
+++ SW/client/scripts/GcodeSender.js
@@ -1,25 +1,31 @@
 var GCODE_SENDER = (function (my) {
 
 
+  // Construct a motion command with 3 angels
   my.move = function(target){
-
-    var x = my.degToRad(target.R0);
-    var y = my.degToRad(target.R1);
-    var z = my.degToRad(target.R2);
-
-    var position = "G1 X"+x+" Y"+y+" Z"+z +" F1000";
-
-    // var position = "G1 Z"+z+" F1000";
+    var x = my.radToDeg(target.R0);
+    var y = my.radToDeg(target.R1);
+    var z = my.radToDeg(target.R2);
+    var position = "G0 X"+x+" Y"+y+" Z"+z;
     my.sendGcode(position);
   }
 
+  // Help commands
+  my.home     = function(){my.sendGcode("$H");}
+  my.settings = function(){my.sendGcode("$$");}
+  my.killLock = function(){my.sendGcode("$X");}
+  my.mode     = function(){my.sendGcode("$C");}
+  my.feedHold = function(){my.sendGcode("!");}
+  my.status   = function(){my.sendGcode("?");}
+
+  // Parse a command to the socket.io
   my.sendGcode = function(str){
     console.log("Send gcode: "+str);
     socket.emit('gcode', str);
   }
 
-
-  my.degToRad = function(radians) {
+  // Convert radians to degrees
+  my.radToDeg = function(radians) {
     return radians * 180 / Math.PI;
   };
 
